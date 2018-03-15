3d32dd7e0d1527c2b361449283fec7ce119aad91
Türenç Engin <turencengin@gmail.com>
2018-03-15 11:47:00 +0300

    gitignore file added.

--- EFTemplateApp/Startup.cs
+++ EFTemplateApp/Startup.cs
@@ -17,7 +17,6 @@ namespace EFTemplateApp
         {
             Configuration = configuration;
         }
-
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
@@ -33,7 +32,7 @@ namespace EFTemplateApp
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            //hi
             app.UseMvc();
         }
     }
