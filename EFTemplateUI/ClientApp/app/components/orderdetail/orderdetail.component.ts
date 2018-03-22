a9561c1db05b49c9bf5c0a50410d116d58c9766f
EmrahDi <emrahdincadam@gmail.com>
2018-03-22 10:19:14 +0300

    button

--- EFTemplateUI/ClientApp/app/components/orderdetail/orderdetail.component.ts
+++ EFTemplateUI/ClientApp/app/components/orderdetail/orderdetail.component.ts
@@ -1,18 +1,26 @@
 import { Component, Inject } from '@angular/core';
 import { Http } from '@angular/http';
 
+export var HttpObject: Http;
+export var BaseUrl: string;
+
 @Component({
     selector: 'orderdetail',
     templateUrl: './orderdetail.component.html'
 })
 export class OrderDetailComponent {
     public customerOrderDetails: CustomerOrderDetailDto[];
-
+    
     constructor(http: Http, @Inject('BASE_URL') baseUrl: string) {
-        http.get(baseUrl + 'api/Order/GetCustomerOrderDetail').subscribe(result => {
+        HttpObject = http;
+        BaseUrl = baseUrl;
+    }
+    public GetOrderDetails() {
+        HttpObject.get(BaseUrl + 'api/Order/GetCustomerOrderDetail').subscribe(result => {
             this.customerOrderDetails = result.json() as CustomerOrderDetailDto[];
         }, error => console.error(error));
     }
+
 }
 
 interface CustomerOrderDetailDto {
