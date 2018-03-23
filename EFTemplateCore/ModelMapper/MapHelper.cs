5bc4cad92b39792e4a26b3fd0b7f77908a699f63
EmrahDi <emrahdincadam@gmail.com>
2018-03-23 10:16:06 +0300

    hashmap added to automapper class

--- EFTemplateCore/ModelMapper/MapHelper.cs
+++ EFTemplateCore/ModelMapper/MapHelper.cs
@@ -1,23 +1,36 @@
 ﻿using AutoMapper;
+using EFTemplateCore.Logging;
+using EFTemplateCore.ServiceLocator;
+using Microsoft.Extensions.Logging;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace EFTemplateCore
+namespace EFTemplateCore.ModelMapper
 {
     public static class MapHelper<TSource, TTarget>
     {
+        /// <summary>
+        /// AutoMapper takes InvalidOperationException exception when initilizes again with the same type name
+        /// One way is to initilize the types on startup of the application or keep the mappings of the types.
+        /// todo:emrahdi there is no thread safe hashset.ConcurrentDictionary must be used
+        /// </summary>
+        static ConcurrentDictionary<string, string> MapDictionary = new ConcurrentDictionary<string, string>();
         public static void CreateMap()
         {
-            try
-            {
-                Mapper.Initialize(cfg => cfg.CreateMap<TSource, TTarget>().ReverseMap());
+            try {
+                string typeName = string.Concat(typeof(TSource).Name, "-", typeof(TTarget).Name);
+                if (!MapDictionary.ContainsKey(typeName)) {
+                    Mapper.Initialize(cfg => cfg.CreateMap<TSource, TTarget>());
+                    MapDictionary.TryAdd(typeName, typeName);
+                }
             }
-            catch(InvalidOperationException ex)
-            {
+            catch (InvalidOperationException ex) {
+                Services.Create<ILog>().LogFormat("Exeption in MapHelper-CreateMap:{0}", LogLevel.Warning, ex);
             }
         }
-        
+
         public static TTarget Convert(TSource source)
         {
             CreateMap();
@@ -28,8 +41,7 @@ namespace EFTemplateCore
         {
             CreateMap();
             List<TTarget> viewList = new List<TTarget>();
-            foreach (TSource source in sourceList)
-            {
+            foreach (TSource source in sourceList) {
                 viewList.Add(Mapper.Map<TSource, TTarget>(source));
             }
             return viewList;
--- Modules.NorthWind.BusinessLogic/Transactions/EmployeeTransactions.cs
+++ Modules.NorthWind.BusinessLogic/Transactions/EmployeeTransactions.cs
@@ -15,38 +15,12 @@ namespace Modules.NorthWind.BusinessLogic.Transactions
         {
             this.northWindUnitOfWork = northWindUnitOfWork;
         }
-
         public EmployeeResponse InsertEmployee(EmployeeRequest request)
         {
             EmployeeResponse response = new EmployeeResponse();
             EmployeeDto employeeDto = request.EmployeeDto;
-            
             EmployeeRepository employeeRepository = northWindUnitOfWork.EmployeeRepository;
             EntityEntry<Employee> employee = employeeRepository.Add(employeeDto.ToEntity<EmployeeDto, Employee>());
-
-            //TODO:turenc-->Yukarıdaki satır automapper ile aşağıdaki işlemi hallediyor.
-            //EntityEntry<Domain.Employee> employee = employeeRepository.Add(new Domain.Employee()
-            //{
-            //    EmployeeId = employeeDto.EmployeeId,
-            //    FirstName = employeeDto.FirstName,
-            //    LastName = employeeDto.LastName,
-            //    Title = employeeDto.Title,
-            //    TitleOfCourtesy = employeeDto.TitleOfCourtesy,
-            //    BirthDate = employeeDto.BirthDate,
-            //    HireDate = employeeDto.HireDate,
-            //    Address = employeeDto.Address,
-            //    City = employeeDto.City,
-            //    Region = employeeDto.Region,
-            //    PostalCode = employeeDto.PostalCode,
-            //    Country = employeeDto.Country,
-            //    HomePhone = employeeDto.HomePhone,
-            //    Extension = employeeDto.Extension,
-            //    Photo = employeeDto.Photo,
-            //    Notes = employeeDto.Notes,
-            //    ReportsTo = employeeDto.ReportsTo,
-            //    PhotoPath = employeeDto.PhotoPath
-            //});
-
             response.EmployeeId = employee.Entity.EmployeeId;
             return response;
         }
